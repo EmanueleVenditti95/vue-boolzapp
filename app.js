bcf7719a3a9a36338276a06cae94a98429386356
EmanueleVenditti95 <vendittiema@gmail.com>
2023-10-26 14:19:58 +0200

    Add - start

---
+++ app.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue;
+
+  createApp({
+    data() {
+      return {
+    
+      }
+    },
+    method: {
+
+    },
+    mounted () {
+        console.log('vue ok')
+    }
+  }).mount('#app')
